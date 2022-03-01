e19783d3bb50c8a94898e138b69f00801f2fed7b
Maruf Ahmed Tushar <53092408+marufahmedtushar@users.noreply.github.com>
2022-03-01 14:28:31 +0600

    Uploaded on 01-mar-2022 2:28 pm
    Uploaded on 01-mar-2022 2:28 pm

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/**
+ *
+ *  sticky navigation
+ *
+ */
+
+let navbar = $(".navbar");
+
+$(window).scroll(function () {
+  // get the complete hight of window
+  let oTop = $(".section1").offset().top - window.innerHeight;
+  if ($(window).scrollTop() > oTop) {
+    navbar.addClass("sticky");
+  } else {
+    navbar.removeClass("sticky");
+  }
+});
+
+
